8fe58bfc5fc582dfb207d09f736e7fc74b9b770a
Vamqueror <liorx444@gmail.com>
2021-07-14 12:37:16 +0300

    14/7/2021 12:37

---
+++ Deck.js
@@ -0,0 +1,121 @@
+const types=["clubs","diamonds","hearts","spades"];
+const values=['ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'];
+
+class Card{
+    constructor(value,type){
+        this.value=value
+        this.type=type
+    }
+    /*
+    get value(){
+        return this.value
+    }
+
+    set value(val){
+        if(values.includes(val))
+            this.value=val
+    }
+
+    get type(){
+        return this.type
+    }
+
+    set type(typ){
+        if(types.includes(typ))
+            return this.type=typ
+    }*/
+}
+
+
+class Deck{
+    constructor(){
+        this.newGame();
+    }
+    
+    newGame(){
+        this.cards=[];
+
+        for(let value=0;value<values.length;value++){
+            for(let type=0;type<types.length;type++){
+                this.cards.push(new Card(values[value],types[type]));
+            }
+        }
+        this.shuffle();
+        this.dealerCards=this.drawInit();
+    }
+
+    shuffle(){
+        for(let i=0;i<this.cards.length;i++){
+            let rng=parseInt(Math.random()*this.cards.length);
+            let temp=this.cards[rng];
+            this.cards[rng]=this.cards[i];
+            this.cards[i]=temp;
+        }
+    }
+
+    print(){
+        for(let i=0;i<this.cards.length;i++){
+            console.log(this.cards[i]+"\n");
+        }
+    }
+
+    draw(){
+        this.shuffleIfEmpty();
+        return this.cards.shift();
+    }
+
+    shuffleIfEmpty()//create a new deck if deck is empty
+    {
+        if(this.cards.length===0)
+            this.newGame();
+    }
+
+    getDeckLength()
+    {
+        return this.cards.length;
+    }
+
+    drawInit()//draw two cards | useful from game init
+    {
+        let hand=[],temp
+        hand.push(this.draw())
+        hand.push(this.draw())
+        return hand
+    }
+
+    dealerTurn()
+    {
+        let myCards=this.dealerCards;
+        while(this.calcScore(myCards)<17){
+            myCards=[...myCards,this.draw()];
+        }
+        return this.dealerCards=myCards;
+    }
+
+    getDealer(){return this.dealerCards;}
+
+    calcScore(cardsArray)
+    {
+        let score=0
+
+        cardsArray.forEach(element => {
+        switch(element.value)
+        {
+        case 'jack':
+        case 'queen':
+        case 'king':
+          score+=10
+          break;
+        case 'ace':
+          score+=11
+          break;
+        default:
+          score+=element.value
+          break;
+      }
+    });
+    return score
+    }
+}
+
+module.exports=Deck
---
+++ ServerModule.js
@@ -0,0 +1,61 @@
+
+const http = require('http')
+const port=4000
+let cardDeck=require('./Deck')
+let deck=new cardDeck();
+
+//create a server object:
+const server=http.createServer((req, res) =>{
+  console.log(req.url)
+  switch(req.url)
+  {
+    case '/Draw':
+      {
+        let cardDrawn=deck.draw()
+        res.write(JSON.stringify(cardDrawn)); //write a response to the client
+        break
+      }
+    case '/DealerCards':
+      {
+        res.write(JSON.stringify(deck.getDealer()));
+        break;
+      }
+    case '/DealerPlay':
+      {
+        res.write(JSON.stringify(deck.dealerTurn()));
+        break;
+      }
+    case '/DeckLength':
+      {
+        res.write(JSON.stringify(deck.getDeckLength()))
+        break
+      }
+    case '/DrawInit':
+      {
+        res.write(JSON.stringify(deck.drawInit()))
+        break
+      }
+    case '/Reset':
+      {
+        deck.newGame()
+        res.write("true");
+        break
+      }
+    default:
+      break
+  }
+  res.end(); //end the response
+})
+
+
+
+server.listen(port,function(error)
+{
+  if(error){
+    console.log('Error found:',error)
+  }
+  else{
+    console.log('Server is up on port '+port)
+    console.log('\n http://localhost:4000/')
+  }
+})
